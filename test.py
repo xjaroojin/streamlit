94af677b4ac502da3370986d80a4502262de2d7f
Jin <91413259+xjaroojin@users.noreply.github.com>
2022-11-05 15:03:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 st.title('Uber pickups in NYC')
 streamlit run uber_pickups.py
-$ streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
-
