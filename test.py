90c58a0db094b9d79687150157ef15b62e6147b4
Jin <91413259+xjaroojin@users.noreply.github.com>
2022-11-05 15:08:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,3 +12,11 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache)")
+@st.cache
+def load_data(nrows):
