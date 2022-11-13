77a7e6d09f70cdd3ff3b3191083dd2d97b8a42c2
Jin <xjaroo.jin.hong@icloud.com>
2022-11-13 19:24:50 +0800

    headers and nasme

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 import streamlit as st
 
-st.title('Uber pickups in NYC')
+st.title('i am behind you')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-st.header('helo')
+st.header('boo!!!')
+st.write('hello there, my fellow potatoes')
